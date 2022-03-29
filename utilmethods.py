81e90e454c336e9cdcd8878db9ca54ec1696c31f
LuisGMM <emaildemiscuentas@gmail.com>
2022-03-29 15:10:37 +0200

    Added a first idea of euler_implicit and added more functionality to newton method
    Newton method is much more polivalent

--- utilmethods.py
+++ utilmethods.py
@@ -7,40 +7,64 @@ import numpy as np
 
 
 
-def newton(composite:'Callable[Callable, float, float, float]', f_dev:'Callable[float]', c:float, err:float, x0:float = 0) -> float:
-    """Newton's method to find roots of the integral of f_dev with integration constant c.
-
-    This method should be used when only the analitycal derivative f_dev of the function is known.
-    composite arg should be composite_trapezoid or composite_simpson callable methods.
+def newton(err:float, f:'Callable[float]' = None, f_dev:'Callable[float]' = None,
+    composite:'Callable[Callable, float, float, float]' = None,  c:float = 0, x0:float = 0, h_err:float = 1e-4) -> float:
+    """Newton's method to find roots of a function.
+    
+    If no `f` is given but `f_dev` and `composite` are, it will compute the roots of the integral of `f_dev` with integration constant c.
+    If `f_dev` is not given, it will be computed from `f` with the mathematical definition of a derivative.
 
     Args:
-        composite (Callable[Callable, float, float, float]): _description_
-        f_dev (Callable[float]): Analytical derivative of the function. It input is the point to be evaluated in.
-        c (float): Integration constant of the integral of f_dev. 
         err (float): Desired error of the method.
+        f_dev (Callable[float], optional): Analytical function to find its roots. Its input is the point to be evaluated in. Defaults to None.
+        f_dev (Callable[float], optional): Analytical derivative of the function. Its input is the point to be evaluated in. Defaults to None.
+        composite (Callable[Callable, float, float, float], optional): Integration method to compute the integral of `f_dev` and find its roots. 
+            It should be `composite_trapezoid` or `composite_simpson` methods. Defaults to None.
+        c (float, optional): Integration constant of the integral of f_dev. Defaults to 0.
         x0 (float, optional): Initial guess of the root. 
             Note that an inadequate first guess could lead to undesired outputs such as no roots or undesired roots.
             Defaults to 0.
+        h_err (float, optional): Finite approximation of 0 to use in the calculation of `f_dev` by its mathematical definition. Defaults to 1e-4.
 
     Returns:
-        float: Root of the function.
+        float|None: Root of the function or None if the algorithm reaches its recursion limit.
     """    
-    iter_value = {0:x0}
-    iter=0
+    def dev(x:float, f:function = f, h_err:float = h_err)-> float:
+        return ( f(x+h_err) - f(x) ) / h_err 
+
+    if (f or composite) and f_dev:
+        if f and composite:
+            warnings.warn('`f`, `f_dev` and `composite` args detected. Only `f` and `f_dev` will be used for sake of precision.') 
+            iteration = lambda iter_idx, iter_dict: iter_dict[iter_idx] - f(iter_dict[iter_idx]) / f_dev(iter_dict[iter_idx])
+
+        elif composite:
+            iteration = lambda iter_idx, iter_dict: iter_dict[iter_idx] - (composite(f_dev, x0, iter_dict[iter_idx], 100_000) + c) / f_dev(iter_dict[iter_idx])
+
+        else:
+            iteration = lambda iter_idx, iter_dict: iter_dict[iter_idx] - f(iter_dict[iter_idx]) / f_dev(iter_dict[iter_idx])
+
+    elif f and f_dev == None:
+        warnings.warn(f'`f_dev` was not given. It will be computed using the derivative definition with `h`={h_err} .') 
+        iteration = lambda iter_idx, iter_dict: iter_dict[iter_idx] - f(iter_dict[iter_idx]) / dev(x=iter_dict[iter_idx], f=f)
+    
+    else:
+        raise type('InadequateArgsCombination', (Exception,), {})('Cannot compute Newton s method with the combination of arguments given. Check the valid combinations.')
+
+    iter, iter_dict = 0, {0:x0}
     limit = sys.getrecursionlimit()
 
     while True:
-        
         if iter + 10 >= limit:
-            warnings.warn('Iteration limit reached without finding any root. Try with other initial guess. Maybe there are no roots.')
+            warnings.warn(f'Iteration limit ({limit}) reached without finding any root. Try with other initial guess or changing the recursion limit. Maybe there are no roots.')
             return 
         
-        iter_value[iter+1] = iter_value[iter] - (composite(f_dev, x0, iter_value[iter], 100000) + c) / f_dev(iter_value[iter])
+        iter_dict[iter+1] = iteration(iter, iter_dict)
+        
+        if abs(iter_dict[iter+1] - iter_dict[iter]) < err:
+            return iter_dict[iter+1]
+        
         iter += 1
 
-        if abs(iter_value[iter] - iter_value[iter-1]) < err:
-            return iter_value[iter]
-        
 
 def composite_trapezoid(f_:'Callable[float]', a:float, b:float, n:float)-> float:
     """Computes the analitical solution of the integral of f from a to b 
@@ -110,4 +134,46 @@ def euler_explicit(f:'Callable[float, float]', y0:float, t0:float, t:float, h:fl
     return u
 
 def euler_implicit(f:'Callable[float, float]', y0:float, t0:float, t:float, h:float)-> np.ndarray:
-    pass
+    """Computes the implicit (backward) Euler method to solve ODEs.
+
+    Args:
+        f (Callable[float, float]): Function depending on y and t in that order.
+            Equivalent to f(y,t).
+        y0 (float): Initial value of the answer.
+            Equivalent to y(t0).
+        t0 (float): Initial time.
+        t (float): Final time.
+        h (float): Separation between the points of the interval.
+
+    Returns:
+        np.ndarray: Numerical solution of the ODE in the interval [t0, t0+h, t-h, t].
+    """
+
+    t_ = np.arange(t0, t+h, h)
+    N = len(t_)
+
+    u = np.zeros_like(t_)
+    u[0] = y0
+        
+    for i in range(N-1):        
+
+        g = lambda y: u[i] + u[y] + h*f(y,t_[i+1])
+
+        u[i+1] = newton(g, 0.01, u[i])
+
+    return u
+
+
+if __name__ == '__main__':
+    
+
+    def test1(arg1 = None, arg2= None): return print(arg1, arg2)
+
+    
+    def test2(f, *args):
+        return f(*args)
+
+
+    test1(1, 2)
+
+    test2(1 ,2)
